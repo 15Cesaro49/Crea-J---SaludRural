8563acc38b5536b8c6180bb354ca218bd23628eb
CELS09 <lopez.saca.carlos8a@gmail.com>
2023-09-09 05:21:03 -0600

    hvhk

--- Avance-CREAJ/HTML/Index.php
+++ Avance-CREAJ/HTML/Index.php
@@ -38,13 +38,16 @@ if (!isset($_SESSION['correo']) || empty($_SESSION['correo'])) {
 <!DOCTYPE html>
 <html lang="es">
   <head>
-  <meta charset="UTF-8" />
+    <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="shortcut icon" href="../Imagenes/favicon.png"/>
     <title>SaludRural</title>
     <link rel="stylesheet" href="../CSS/traductor.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   </head>
